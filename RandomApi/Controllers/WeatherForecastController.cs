9fc909e7089ce7fa3f2ee51bfd4eff8897163a89
VasylZavhorodnii <vasilevszavgor@gmail.com>
2021-11-16 18:27:39 +0200

    work

---
+++ RandomApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            var rng = new Random();
+            return rng.Next(8,800);
+        }
+    }
+}
