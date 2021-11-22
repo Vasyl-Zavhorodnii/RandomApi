6bb6122d6e379b3d4d92e277e96b7b986162237e
VasylZavhorodnii <vasilevszavgor@gmail.com>
2021-11-22 14:50:17 +0200

    Reverse in LINQ

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона", "Руслан", "Встав", "Яйця"};
+
+            var selectedTeams = from t in teams 
+                                where t.ToUpper().Length>=5 
+                                orderby t  
+                                select t; 
+
+            foreach (string s in selectedTeams)
+                Console.WriteLine(string.Join(",", s).Reverse().ToArray());
+            Console.ReadLine();
+        }
+    }
+}
